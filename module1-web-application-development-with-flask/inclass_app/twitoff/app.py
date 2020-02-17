aba0e8663e98fd4a78d0892e219a65d0f4317a25
Skantastico <Skantastico@gmail.com>
2020-02-16 21:57:36 -0500

    Add form and issue post request

--- module1-web-application-development-with-flask/inclass_app/twitoff/app.py
+++ module1-web-application-development-with-flask/inclass_app/twitoff/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    # return "Hello World!"
+    return render_template("homepage.html")
 
 
 @app.route("/about")
@@ -24,7 +25,7 @@ def users():
     return jsonify(users)
 
 
-@app.route("users/create", methods=["POST"])
+@app.route("/users/create", methods=["POST"])
 def create_user():
     print("CREATING A NEW USER...")
     print("FORM DATA:", dict(request.form))
