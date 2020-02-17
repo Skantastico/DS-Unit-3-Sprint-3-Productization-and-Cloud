eaa19f3966845e9e44e69f6768bbdfd8925ee327
Skantastico <Skantastico@gmail.com>
2020-02-17 11:48:47 -0500

    Make sense of the basilica code

--- module2-consuming-data-from-an-api/twitoff/basilica_service.py
+++ module2-consuming-data-from-an-api/twitoff/basilica_service.py
@@ -12,7 +12,14 @@ sentences = [
     "This is a similar sentence!",
     "I don't think this sentence is very similar at all...",
 ]
-with basilica.Connection(BASILICA_API_KEY) as c:
-    embeddings = list(c.embed_sentences(sentences))
+
+connection = basilica.Connection(BASILICA_API_KEY)
+
+embeddings = list(connection.embed_sentences(sentences))
 
 print(embeddings)
+
+for emb in embeddings:
+    print(type(emb))
+    print(emb)
+    print("------------")
