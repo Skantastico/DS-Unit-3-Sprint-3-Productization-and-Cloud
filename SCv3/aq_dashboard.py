79e5ef093aa03146abd8efd3c97735aa52f4bc3e
Skantastico <Skantastico@gmail.com>
2020-02-25 14:31:11 -0500

    Got part 4 working!

--- SCv3/aq_dashboard.py
+++ SCv3/aq_dashboard.py
@@ -14,6 +14,13 @@ DB = SQLAlchemy(APP)
 api = openaq.OpenAQ()
 
 
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return '< Time {} -- Value {} >'.format(self.datetime, self.value)
 
 
 def get_data():
@@ -26,9 +33,29 @@ def get_data():
     return date_value_tuples
 
 
+def get_tuples(get_data):
+    for tuple in get_data:
+        add_db_records = Record(datetime=tuple[0], value=tuple[1])
+        DB.session.add(add_db_records)
+
+
 @APP.route('/')
 def root():
     """Base view."""
-    results = get_data()
-    return str(results)
-    # return render_template("base.html", title=results)
+    records = Record.query.filter(Record.value>=10).all()
+    return str(records)
+
+    # this is the code I used for part 1 and 2, depricated
+    # results = get_data()
+    # return str(results)
+    # # return render_template("base.html", title=results)
+
+@APP.route('/refresh')
+def refresh():
+    """Pull fresh data from OpenAQ and replace existing."""
+    DB.drop_all()
+    DB.create_all()
+    tuples = get_data()
+    get_tuples(tuples)
+    DB.session.commit()
+    return 'Data refreshed!'
